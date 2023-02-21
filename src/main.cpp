fa8fe1bd5c99e72f1fb0e2de54cc924bf78acea5
OlivierQuintal <oliquintal33@gmail.com>
2023-02-21 18:36:00 -0500

    code de marde

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <mbed.h>
 
 
+#define STEPS_PER_REVOLUTION 200  // 200 steps par revolution (1.8 degre)
+#define degParSecMax 720      // degré par seconde max
+
 DigitalOut stepPin(p21);
 DigitalOut dirPin(p22);
 DigitalOut En(p23);
-
-#define STEPS_PER_REVOLUTION 200
-int vitesse = 0;
-
-
 AnalogIn  pot(p15);
+
+int vitesse = 0;    // valeur entre 0 et 720
+
+
+
 //const int stepsPerRevolution = 200;
 int i;
 float timeStep = 0.0025;
@@ -31,30 +34,24 @@ int main() {
   //t.attach(fonctionStep, 0.1);
   t.attach(&fonctionStep, 0.0025);
   while (true) {
-
-    
-
-
-    vitesse = pot.read() * 720;     // valeur entre 0 et 720
+    vitesse = pot.read() * degParSecMax;     // valeur entre 0 et 720
 
     printf("%d\n\r",vitesse);
     //vitesseAngleParSeconde();
     step = (vitesse * (STEPS_PER_REVOLUTION / 360))/2;  
     printf("%d\n\r",step);
     timeStep = 1 / step;
-    printf("%d\n\r",step);
-
+    printf("%d\n\r",timeStep);
 
   }
 }
-
 
 
 void fonctionStep (void)
 {
   stepPin = !stepPin;
   //vitesseAngleParSeconde();
-  t.attach(&fonctionStep, 0.0025);
+  t.attach(&fonctionStep, timeStep);
 
 }
 
@@ -65,10 +62,5 @@ void vitesseAngleParSeconde (void)
   //printf("%d\n\r",timeStep * 1000);          
 }
 
-
-// 360 deg pour 200 steps 
-// 1 deg pour 200/360 steps
-// 1 step pour 360/200 deg
-// 1 step pour 0.9 deg
 
 
